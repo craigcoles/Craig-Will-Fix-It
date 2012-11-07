002b0efbb73a67b217a3329f895f4c0f1966f795
Craig Coles <craigc@wearebeef.co.uk>
2012-11-07 17:32:59 +0000

    New version

--- javascripts/FixIt.js
+++ javascripts/FixIt.js
@@ -21,37 +21,53 @@
 	
 	function initMyBookmarklet() {
 		(window.myBookmarklet = function() {
-      var jimmy=function(){
-        
-       for(var img=document.images,i=0;i<img.length;i++){
-          
+		  
+		  var replaceWithJimmy = function() {
+        for(var img=document.images,i=0;i<img.length;i++){
+
           var imageSrc = img[i].src.match('savile|Savile');
           var imageAlt = img[i].alt.match('savile|Savile');
-          
+
           if (imageSrc || imageAlt) {
-            
-            var width = img[i].width; 
+
+            var width =  img[i].width;
             var height = img[i].height;
-            img[i].style.height = height;
-            img[i].style.width = width;
-            
+
+            $(img[i]).wrap('<div class="imgLiquid" style="width:' + width + 'px; height:' + height + 'px" />');
+
             if(width&&height) {
               img[i].setAttribute("width", width);
               img[i].setAttribute("height", height);
-              img[i].src='http://www.craigcoles.co.uk/lab/savile/savile-glass.gif';
-              img[i].style.background="url('http://www.craigcoles.co.uk/lab/savile/nspcc-logo.jpg') no-repeat center center";
+              img[i].src='http://www.craigcoles.co.uk/lab/savile/nspcc-logo.jpg';
             }
-            
           }
-      }
-      
+        }		    
+		  };
+		  
+      var jimmy=function(){
+       
+        // ImgLiquid Lib CSS
+        var imgLiquidcss   = document.createElement("link");
+        imgLiquidcss.rel  = "stylesheet";
+        imgLiquidcss.type  = "text/css";
+        imgLiquidcss.href   = "http://www.craigcoles.co.uk/lab/savile/imgLiquid.js.css";
+        document.body.appendChild(imgLiquidcss);
+
+        $.getScript("http://www.craigcoles.co.uk/lab/savile/imgLiquid-min.js", function(data, textStatus, jqxhr) {
+          console.log('imgLiquid loaded');
+
+          replaceWithJimmy();
+
+          $(".imgLiquid").imgLiquid({fill:false});
+        });
+        
       return false
       
     };
     
     jimmy();
-    
-		})();
-	}
-
+        
+    })();
+  }
+  
 })();
